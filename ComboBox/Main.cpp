d3e0dbf046e7ef5d88ea384e6c1cd39f060a71a9
Talavidan88 <talavidan88@gmail.com>
2016-03-19 14:30:57 +0200

    Added files via upload

--- ComboBox/Main.cpp
+++ ComboBox/Main.cpp
@@ -1,6 +1,19 @@
+#include <iostream>
+#include <string>
+#include "ComboBox.h"
+#include "../ConsoleSingleton/ConsoleSingleton.h"
+#include "../TextBox/TextBox.h"
 
 int main(int argc, char** argv)
 {
-	int i;
+	std::vector<std::string> bla = { "bla","blals","abl","oddfl","csl","aa","b" };
+	ComboBox* mcb = new ComboBox(20, 10, bla);
+	TextBox* tb = new TextBox(30, 10, 10, 10);
+	mcb->Delete(0);
+	mcb->Delete("ff");
+	ConsoleSingleton::GetInstance()->Sign(mcb);
+	ConsoleSingleton::GetInstance()->Sign(tb);
+	ConsoleSingleton::GetInstance()->Listen();
+	delete mcb;
 	return 0;
 }
